2ab26ae229765476a8b22c6a360185a1158210f8
ReHong <honganthony20003@gmail.com>
2025-05-28 13:23:08 -0400

    Updated Sqlite working

---
+++ APCAssignment3-hong/Student.cpp
@@ -0,0 +1,46 @@
+#include "Student.h"
+#include "User.h"
+#include "Admin.h"
+#include "Instructor.h" 
+
+Student::Student(string first, string last, int ID, string e) : User(first,last,ID,e) {
+
+}
+
+//Method
+
+string Student::search_course() {
+
+	string course;
+	cout << "Enter course name: " << endl;
+	cin >> course;
+	return course;
+}
+
+string Student::add_course() {
+
+	string course;
+	cout << "Enter course name you want to add: " << endl;
+	cin >> course;
+	return course;
+}
+
+string Student::drop_course() {
+
+	string course;
+	cout << "Enter course name you want to drop: " << endl;
+	cin >> course;
+	return course;
+}
+
+string Student::print_course() {
+	string print_course;
+	cout << "This is the printed course" << endl;
+	cin >> print_course;
+	return print_course;
+}
+
+//Destructor
+Student::~Student() {
+
+}
---
+++ APCAssignment3-hong/User.cpp
@@ -0,0 +1,65 @@
+#include "User.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+User::User(string first, string last, int ID) {
+
+
+	//Attributes
+	first_name = first;
+	last_name = last;
+	ID_num = ID;
+
+}
+
+//Method
+string User::show_first() {
+
+	return first_name;
+
+}
+
+string User::show_last() {
+
+	return last_name;
+
+}
+
+int User::show_ID() {
+
+	return ID_num;
+
+}
+
+string User::show_email()
+{
+	return email;
+}
+void User::set_email(string e)
+{
+	email = e;
+}
+
+void User::set_ID(int ID) {
+	ID_num = ID;
+}
+
+void User::set_first(string first) {
+	first_name = first;
+}
+
+void User::set_last(string last) {
+	last_name = last;
+}
+
+
+//Destructor
+User::~User() {
+
+}
